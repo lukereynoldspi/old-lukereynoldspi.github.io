642a36f1dfc2c4eec7d8605964b3c33c7ef9cad6
Luke <lukereynoldspi@gmail.com>
2018-10-08 00:21:28 -0700

    added python folder and tested some python stuff

---
+++ CS_160/python/trash.py
@@ -0,0 +1,15 @@
+import random
+print ("Hello World!")
+music = ["ELO", "Bowie", "Parsons" ]
+x=0
+print (music[0])
+print (music[1])
+print (music[2])
+print (len(music))
+if x==0:
+    music.append("Rogers")
+print (music[3])
+if x==0:
+    music.remove("Parsons")
+print (music)
+print (music[2])
