c0f0e2f644c1bbe50e2bfcafd654ab72c7141d63
Luke <lukereynoldspi@gmail.com>
2018-10-24 11:05:44 -0700

    added color switch

--- CS_160/p5.js/sketch.js
+++ CS_160/p5.js/sketch.js
@@ -1,18 +1,27 @@
 function setup() {
-  createCanvas(1000, 1000);
+  createCanvas(2000, 1000);
   // put setup code here
 }
 
 function draw() {
+ 
   line(240, 300, 240, 568);
-  line(240,568,320,568)
+  line(240, 568, 320, 568);
+
   
   if (mouseIsPressed) {
-    fill(0);
+    fill(199, 21, 133);
+    line(0,0, mouseX,mouseY);
+    stroke(199, 21, 133);
+    strokeWeight(4);
     } else {
+    line(2000,1000, mouseX,mouseY);
+    stroke(0,0,0);
+    strokeWeight(4);
     fill(255);
     }
-    rect(mouseX, mouseY, 30, 30);
+    rect(mouseX, mouseY, 120, 10);
     strokeWeight(4);
+    
   // put drawing code here
 }
