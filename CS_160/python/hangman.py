6266f1f6c7b98a15e17a098f5db272341c436523
Luke <lukereynoldspi@gmail.com>
2018-10-31 00:21:45 -0700

    finished hangman, half assed mastermind

--- CS_160/python/hangman.py
+++ CS_160/python/hangman.py
@@ -1,24 +1,112 @@
 import random
-def random_word():
-    words = random.randint(1,5)
-    if words == 1:
-        words = "APPLE"
-    if words == 2:
-        words = "CHAIR"
-    if words == 3:
-        words = "SPICY"
-    if words == 4:
-        words = "GAMES"
-    if words == 5:
-        words = "STAIN"
-def user_input():
-    input()
-def hangman():
-    print ("  ______")
-    print ("  l     l")
-    print ("  O     l")
-    print ("--l--   l")
-    print (" ( )    l")
-    print ("        l")
-    print ("---------")
+def game():
+    words = ["APPLE","CHAIR","SPICY","GAMES","STAIN","SMART","PITCH","STAMP"]
+    wordchoice = random.choice(words)
+    mistakes = 0
+    print("Welcome to hangman! the max amount of letters in the word is 5.")
+    #def keyword():
 
+
+    def printhangman():
+        if mistakes == 0:
+            print ("  ______")
+            print ("  l     l")
+            print ("        l")
+            print ("        l")
+            print ("        l")
+            print ("        l")
+            print ("---------")
+        if mistakes == 1:
+            print ("  ______")
+            print ("  l     l")
+            print ("  O     l")
+            print ("        l")
+            print ("        l")
+            print ("        l")
+            print ("---------")
+        if mistakes == 2:
+            print ("  ______")
+            print ("  l     l")
+            print ("  O     l")
+            print ("  l     l")
+            print ("        l")
+            print ("        l")
+            print ("---------")
+        if mistakes == 3:
+            print ("  ______")
+            print ("  l     l")
+            print ("  O     l")
+            print ("--l     l")
+            print ("        l")
+            print ("        l")
+            print ("---------")
+        if mistakes == 4:
+            print ("  ______")
+            print ("  l     l")
+            print ("  O     l")
+            print ("--l--   l")
+            print ("        l")
+            print ("        l")
+            print ("---------")
+        if mistakes == 5:
+            print ("  ______")
+            print ("  l     l")
+            print ("  O     l")
+            print ("--l--   l")
+            print (" (      l")
+            print ("        l")
+            print ("---------")
+        if mistakes == 6:
+            print ("  ______")
+            print ("  l     l")
+            print ("  O     l")
+            print ("--l--   l")
+            print (" ( )    l")
+            print ("        l")
+            print ("---------")
+            print ("GAME OVER")
+            print ("The word was:")
+            print (wordchoice)
+            print ("Press enter to play again!") 
+            input() 
+            game()
+    printhangman()
+    #def guessinput():
+    guessedletters = []
+    while mistakes < 6:
+        #guessinput()
+        print ("Press 1 to guess the word. Guess a letter: ")
+        print("---------------------------------------------")
+        letterguess = input()
+        if letterguess == "1":
+            print("---------------------------------------------")
+            print("What is the word?")
+            print("---------------------------------------------")
+            word = input()
+            word = word.upper()
+            if word == wordchoice:
+                print("---------------------------------------------")
+                print("CONGRATULATIONS")
+                print("YOU WIN")
+                print("Press enter to play again!")
+                print("---------------------------------------------")
+                input()
+                game()
+        letterguess = letterguess.upper()
+        guessedletters.append(letterguess)
+        print ("Guessed Letters:")
+        print (guessedletters)
+        
+        if letterguess in wordchoice:
+            printhangman()
+            print ("Yes")
+            print("---------------------------------------------")
+        if letterguess not in wordchoice:
+            mistakes = mistakes + 1
+            printhangman()
+            print ("No")
+            print("---------------------------------------------")
+            #guessinput()
+        
+    print(wordchoice)   
+game()
--- CS_160/python/mastermind.py
+++ CS_160/python/mastermind.py
@@ -1,3 +1,92 @@
 import random
-# def random_color_set():
-    
+import sys
+def mastermind():
+    game = 1
+    guesses = 8
+    colors = ["R", "G", "B", "W",]
+    spotone = random.choice(colors)
+    spottwo = random.choice(colors)
+    spotthree = random.choice(colors)
+    spotfour = random.choice(colors)
+    colorlist = []
+    colorlist.append(spotone)
+    colorlist.append(spottwo)
+    colorlist.append(spotthree)
+    colorlist.append(spotfour)
+
+
+
+    print("Welcome to Mastermind! You have 8 guesses to find the correct color combo,")
+    print("there are 4 colors, Red, Green, Blue, White.")
+    print("To input your answer, put the first letter of each color in the order")
+    print("you want, such as RGBW for example. B means right color wrong position,")
+    print("C means right position and color, and N means the color is not in the combo.")
+    print("------Good Luck!------")
+    while game == 1: 
+        guess = input()
+        guess = guess.upper()
+        guess = list(guess)
+        print (guess)
+        #print (colorlist)
+        if len(guess) == len(colorlist):
+            if guess == colorlist:
+                print ("YOU WIN!")
+                print ("Would you like to play again? Y/N")
+                answer = input()
+                answer = answer.upper()
+                if answer == "Y":
+                    mastermind()
+                if answer == "N":
+                    end()
+                if answer != "Y" or "N":
+                    print ("Sorry, what?")
+
+            for item in guess[0]:
+                if item in colorlist[0]:
+                    print ("C")
+                if item in colorlist [1-3] and item not in colorlist[0]:
+                    print ("B")
+                if item not in colorlist[0-3]:
+                    print("N")
+            for item in guess[1]:
+                if item in colorlist[1]:
+                    print ("C")
+                if item in colorlist[0] or colorlist[2-3] and item not in colorlist[1]:
+                    print ("B")
+                if item not in colorlist[0-3]:
+                    print("N")
+            for item in guess[2]:
+                if item in colorlist[2]:
+                    print ("C")
+                if item in colorlist[0-1] or colorlist[3] and item not in colorlist[2]:
+                    print ("B")
+                if item not in colorlist[0-3]:
+                    print("N")
+            for item in guess[3]:
+                if item in colorlist[3]:
+                    print ("C")
+                if item in colorlist[0-2] and item not in colorlist[3]:
+                    print ("B")
+                if item not in colorlist[0-3]:
+                    print("N")
+                    
+
+            #if guess not in colorlist:
+            #print ("None match :(")
+            guesses = guesses - 1
+            print ("You have")
+            print (guesses)
+            print ("guesses left!")
+            if guesses == 0:
+                print("Game Over")
+                print(colorlist)
+                print("Press enter to play again! ")
+                input("")
+                mastermind()
+        if len(guess) != len(colorlist):
+            print ("please select 4 colors.")
+def end():
+    print ("Thanks for playing!")
+    input()
+    end()
+mastermind()
