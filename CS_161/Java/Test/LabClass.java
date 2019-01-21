74c6f2dcace587d22bc523f95b160a6822012d52
Luke <lukereynoldspi@gmail.com>
2019-01-20 22:25:41 -0800

    added chapter 2

--- CS_161/Java/Test/LabClass.java
+++ CS_161/Java/Test/LabClass.java
@@ -7,27 +7,13 @@
  */
 public class LabClass
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class LabClass
-     */
-    public LabClass()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+    // private
+    // class level variables, fields, attributes, properties
+    
+    // constructor(s)
+    
+    // action methods, do something with the object
+    // public either return void or a type, parameterlist
+    
+    
 }
---
+++ CS_161/Java/Test/Widget.java
@@ -0,0 +1,38 @@
+
+/**
+ * Write a description of class Widget here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Widget
+{
+    // instance variables - replace the example below with your own
+    private int x;
+    private String color:
+    /**
+     * Constructor for objects of class Widget
+     */
+    public Widget()
+    {
+        weight = 10;
+        color = "red";
+    }
+    public Widget()
+    {
+        weight = 10;
+        color = "red";
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
