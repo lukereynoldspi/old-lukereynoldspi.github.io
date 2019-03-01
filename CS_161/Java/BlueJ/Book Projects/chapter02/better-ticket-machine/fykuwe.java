3af74b031a8ee432816e03e4710fc7496d5e8d19
Luke <lukereynoldspi@gmail.com>
2019-03-01 15:20:12 -0800

    d

---
+++ CS_161/Java/BlueJ/Book Projects/chapter02/better-ticket-machine/fykuwe.java
@@ -0,0 +1,36 @@
+
+/**
+ * Write a description of class fykuwe here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class fykuwe
+{
+    // instance variables - replace the example below with your own
+    private int x;
+    
+    private String spicy;
+
+    /**
+     * Constructor for objects of class fykuwe
+     */
+    public fykuwe()
+    {
+        // initialise instance variables
+        x = 0;
+        spicy = "oof";
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public sampleMethod()
+    {
+        // put your code here
+        return spicy;
+    }
+}
