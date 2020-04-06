9883bba41a4df84e4f400b6f1b262a05efd01db4
Luke <lukereynoldspi@gmail.com>
2020-04-06 00:12:21 -0700

    E

---
+++ CS_162/Java/Test/LabClass.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class LabClass here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class LabClass
+{
+    // private
+    // class level variables, fields, attributes, properties
+    
+    // constructor(s)
+    
+    // action methods, do something with the object
+    // public either return void or a type, parameterlist
+    
+    
+}
